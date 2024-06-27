a16c8872ce5260cbcbc652e93c7191c421602414
Jamkadev007 <jamshidbekbekmetov@gmail.com>
2024-06-27 21:35:33 +0500

    Category added

---
+++ src/main/java/com/braindevs/entity/CategoryEntity.java
@@ -0,0 +1,26 @@
+package com.braindevs.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "category")
+public class CategoryEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "created_date")
+    private LocalDateTime createdDate = LocalDateTime.now();
+
+
+}
